32235baa579bbd622a9c8f8e2b5895dad79a8348
MrJovanovic13 <mrjovanovic13@yahoo.com>
2024-01-22 00:23:20 +0100

    Implement heartbeat for SSE

--- src/main/kotlin/com/mrjovanovic/threadsnexus/config/DeviceEventsSinkConfig.kt
+++ src/main/kotlin/com/mrjovanovic/threadsnexus/config/DeviceEventsSinkConfig.kt
@@ -1,10 +1,18 @@
 package com.mrjovanovic.threadsnexus.config
 
+import com.mrjovanovic.threadsnexus.model.Device
 import com.mrjovanovic.threadsnexus.model.DeviceEvent
+import com.mrjovanovic.threadsnexus.model.enumeration.DeviceStatus
+import com.mrjovanovic.threadsnexus.model.enumeration.DeviceType
+import com.mrjovanovic.threadsnexus.model.enumeration.Severity
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.EventListener
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Sinks
+import java.time.Duration
+import java.time.Instant
 
 @Configuration
 class DeviceEventsSinkConfig {
@@ -18,5 +26,18 @@ class DeviceEventsSinkConfig {
     fun deviceEventsFlux(): Flux<DeviceEvent> {
         return deviceEventsSink().asFlux()
             .cache(0)
+    }
+
+    @EventListener(ApplicationReadyEvent::class)
+    fun initializeSSEHeartbeat() {
+        Flux.interval(Duration.ofSeconds(10))
+            .doOnNext { emitHeartbeatEvent() }
+            .subscribe()
+    }
+
+    private fun emitHeartbeatEvent() {
+        val device = Device("BACKEND_SERVER", "BACKEND_SERVER", DeviceType.UNKNOWN, DeviceStatus.ONLINE, null, null)
+        val deviceEvent = DeviceEvent(Instant.now().toString(), "HEARTBEAT", null, Instant.now(), Severity.LOW, device)
+        deviceEventsSink().tryEmitNext(deviceEvent)
     }
 }
